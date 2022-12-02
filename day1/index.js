099d7117a64ba92ce778fb8e6d20193837fdf04a
Melissa Schultz <melissaschultz@northwesternmutual.com>
2022-12-02 10:15:52 -0600

    Fix Day 1

--- day1/index.js
+++ day1/index.js
@@ -4,7 +4,7 @@ const input = fs.readFileSync('/Users/melissaschultz/Documents/Projects/AdventOf
 function createCalorieGroupTotalArray (arr) {
   const calorieGroupTotals = []
   let total = 0
-  
+
   for(let i=0; i<arr.length; i++) {
     if (arr[i] !== '') {
       total = total + parseInt(arr[i])
@@ -13,6 +13,7 @@ function createCalorieGroupTotalArray (arr) {
       total = 0
     }
   }
+  calorieGroupTotals.push(total)
   return calorieGroupTotals
 }
 
@@ -21,7 +22,7 @@ exports.findHighestCalories = function(arr) {
   // let total = 0
   let mostCalories = 0
 
-  
+
   // for(let i=0; i<arr.length; i++) {
   //   if (arr[i] !== '') {
   //     total = total + parseInt(arr[i])
@@ -36,7 +37,7 @@ exports.findHighestCalories = function(arr) {
       mostCalories = value
     }
   })
-  
+
   return mostCalories
 }
 
@@ -44,50 +45,22 @@ exports.findHighestCalories = function(arr) {
 
 exports.topThree = function(arr) {
   calorieGroupTotals = createCalorieGroupTotalArray(arr)
-  let totalCalories = 0
-  // let topThree = [0,0,0] // [24000, 11000, 10000]
-  const topThree = calorieGroupTotals.sort((a, b) => a - b)
+  // let totalCalories = 0
+  // const topThree = calorieGroupTotals.sort((a, b) => a - b).slice(-3)
+  const topThree = calorieGroupTotals.sort((a, b) => b - a)
   // < less than
   // <= less than or equal to
   // > greater than
   // >= greater than or equal to
 
 
-  // calorieGroupTotals.forEach( value => {
-  //   if (value > topThree[0] && value <= topThree[1] && value <= topThree[2]) {
-  //     topThree[0] = value
-  //   } else if (value > topThree[0] && value <= topThree[1]) {
-  //     let temp = topThree[0]
-  //     let temp2 = topThree[1]
-  //     topThree[0] = value 
-  //     topThree[1] = temp
-  //     topThree[2] = temp2
-  //   } else if (value > topThree[0]) {
-  //     let temp = topThree[0]
-  //     topThree[0] = value 
-  //     topThree[1] = temp
-  //   }
-  // })
-
-  topThree.forEach((value, i) => {
-    let count = 0
-
-    if (count < 3) {
-      if (value != arr[arr.length - i]) { // to handle duplicate values
-        value = arr[arr.length - i];
-        count++;
-      }
-    }
-  })
-
-
 console.log(calorieGroupTotals)
 
-  topThree.forEach( value => {
-    totalCalories += value
-  })
-  console.log({ topThree, totalCalories })
-  return totalCalories
+  // topThree.forEach( value => {
+  //   totalCalories += value
+  // })
+
+  return topThree[0] + topThree[1] + topThree[2]
 }
 
-// console.log(this.topThree(input))
+console.log(this.topThree(input))
