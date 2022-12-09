603a0c7b94fcc4a17a0a900f9c2a35850341cd22
Missy Schultz <creativedarkness@wi.rr.com>
2022-12-08 20:48:33 -0600

    Add complete Day 4 Part One Start Part Two

--- day4/dayFourt.spec.js
+++ day4/dayFourt.spec.js
@@ -1,10 +1,18 @@
 const partOne = require('./part-one.js')
+const partTwo = require('./part-two.js')
 
 describe('Day 4', () => {
   it('should return the number of assignment pairs does one range fully contain the other', () => {
     const expected = 2
 
-    const actual = partOne('DayFourTestData.txt')
+    const actual = partOne('dayFourTestData.txt')
+    expect(actual).toEqual(expected)
+  })
+
+  it('should return the number of assignment pairs does partially contain the other', () => {
+    const expected = 4
+
+    const actual = partTwo('dayFourTestData.txt')
     expect(actual).toEqual(expected)
   })
  })
--- day4/part-two.js
+++ day4/part-two.js
@@ -5,13 +5,15 @@ exports = module.exports = (file, part) => {
   const input = fs.readFileSync(path.resolve(__dirname, `./${file}`), 'utf-8')
     .split('\n')
     .map(assignment => assignment.split(',').map(range => range.split('-').map(num => parseInt(num))))
+
     
-    let fullyEncased = 0
+    let partiallyEncased = 0
 
     input.forEach(assignment => {
       [[firstStart, firstEnd], [secondStart, secondEnd]] = assignment
-      if (firstStart <= secondStart && firstEnd >= secondEnd || secondStart <= firstStart && secondEnd >=firstEnd) fullyEncased += 1
+      if (firstEnd >= secondStart || secondStart <= firstEnd) partiallyEncased += 1
     })
 
-  return fullyEncased
+    console.log(partiallyEncased)
+  return partiallyEncased
 }
