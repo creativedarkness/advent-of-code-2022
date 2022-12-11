aae5aa3f6d88e2afdfcbfd0074b8e54d302d38f9
Missy Schultz <creativedarkness@wi.rr.com>
2022-12-11 11:45:22 -0600

    Add finished Day 4

--- day4/day-four.js
+++ day4/day-four.js
@@ -1,17 +1,28 @@
 const fs = require('fs')
 const path = require('path')
 
-exports = module.exports = (file, part) => {
+exports = module.exports = (file, partTwo) => {
   const input = fs.readFileSync(path.resolve(__dirname, `./${file}`), 'utf-8')
     .split('\n')
     .map(assignment => assignment.split(',').map(range => range.split('-').map(num => parseInt(num))))
     
     let fullyEncased = 0
+    let partiallyEncased = 0
 
     input.forEach(assignment => {
       [[firstStart, firstEnd], [secondStart, secondEnd]] = assignment
       if (firstStart <= secondStart && firstEnd >= secondEnd || secondStart <= firstStart && secondEnd >=firstEnd) fullyEncased += 1
     })
 
-  return fullyEncased
+    input.forEach(assignment => {
+      [[firstStart, firstEnd], [secondStart, secondEnd]] = assignment
+      
+      if ((firstStart <= secondStart && firstEnd >= secondEnd)
+      || (firstStart >= secondStart && firstEnd <= secondEnd)
+      || (firstStart <= secondEnd && secondStart <= firstEnd)) {
+      partiallyEncased += 1
+    }
+    })
+
+  return partTwo ? partiallyEncased : fullyEncased
 }
--- day4/dayFour.spec.js
+++ day4/dayFour.spec.js
@@ -1,18 +1,18 @@
-const partOne = require('./part-one.js')
+const dayFour = require('./day-four.js')
 const partTwo = require('./part-two.js')
 
 describe('Day 4', () => {
   it('should return the number of assignment pairs does one range fully contain the other', () => {
     const expected = 2
 
-    const actual = partOne('dayFourTestData.txt')
+    const actual = dayFour('dayFourTestData.txt')
     expect(actual).toEqual(expected)
   })
 
   it('should return the number of assignment pairs does partially contain the other', () => {
     const expected = 4
 
-    const actual = partTwo('dayFourTestData.txt')
+    const actual = dayFour('dayFourTestData.txt', true)
     expect(actual).toEqual(expected)
   })
  })
